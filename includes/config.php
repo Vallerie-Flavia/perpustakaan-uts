69e5e8eeb13845861d586414d7730a55da8dcd4a
Vallerie Flavia Aurelia <2411500054@mahasiswa.atmaluhur.ac.id>
2025-11-14 12:40:58 +0700

    add: config.php

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// user pass database
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'db_perpustakaan';
+
+// membuat koneksi
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// error handling
+if ($mysqli->connect_error) {
+    die("Koneksi gagal: " . $mysqli->connect_error);
+
+}
+?>
